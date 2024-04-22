05438c3f7824749b568f44b4caffc49dd7bd9126
Kevin-Sean-Chen <kschen@princeton.edu>
2024-04-22 17:37:06 -0400

    LIF2ctmc script

--- LIF2ctmc.py
+++ LIF2ctmc.py
@@ -26,7 +26,7 @@ matplotlib.rc('ytick', labelsize=20)
 # Simulation parameters
 N = 3
 dt = 0.1  # time step in milliseconds
-timesteps = 300000  # total simulation steps
+timesteps = 1000000  # total simulation steps
 lt = timesteps*1
 
 # Neuron parameters
@@ -42,9 +42,24 @@ synaptic_weights = np.array([[0, 1, -2],  # Neuron 0 connections
                              [1, 0, -2],  # Neuron 1 connections
                              [1, 1,  0]])*20  #20  # Neuron 2 connections
 
+# cyclic circuit
+# synaptic_weights = np.array([[0, 0, 1],  # Neuron 0 connections
+#                               [1, 0, 0],  # Neuron 1 connections
+#                               [0, 1, 0]])*20  #20  # Neuron 2 connections
+
+# common circuit
+# synaptic_weights = np.array([[0, 0, 0],  # Neuron 0 connections
+#                               [1, 0, 0],  # Neuron 1 connections
+#                               [1, 0, 0]])*20  #20  # Neuron 2 connections
+
+# chain circuit
+synaptic_weights = np.array([[0, 0, 0],  # Neuron 0 connections
+                              [1, 0, 0],  # Neuron 1 connections
+                              [0, 1, 0]])*20  #20  # Neuron 2 connections
+
 S = synaptic_weights*1
 np.fill_diagonal(S, np.zeros(3))
-noise_amp = 1.5
+noise_amp = 1.5  # 2.0 or 2.5 (1.5, 3.0)
 
 # Synaptic filtering parameters
 tau_synaptic = 5.0  # synaptic time constant
@@ -111,8 +126,8 @@ Cp_condition = np.ones(dofs_all)
 
 # %% inference
 minisi = compute_min_isi(firing)
-adapt_window = 150 #int(minisi*10)  #100
-spk_states, spk_times = spk2statetime(firing, adapt_window)  # embedding states
+adapt_window = 200 #int(minisi*10)  #100
+spk_states, spk_times = spk2statetime(firing, adapt_window, lt=lt)  # embedding states
 tau,C = compute_tauC(spk_states, spk_times)  # emperical measurements
 tau_, C_ = tau/lt, C/lt # correct normalization
 observations = np.concatenate((tau_, C_.reshape(-1))) 
@@ -131,13 +146,14 @@ M_inf, pi_inf = param2M(param_temp)
 
 # %% test CTMC
 M_inf = (C_/tau_[:,None]) ### hijack here
+# M_inf,_ = param2M(np.random.rand(len(param_temp)))  ### random control!!
 
 np.fill_diagonal(M_inf, np.zeros(nc))
 Q = M_inf*1 
 np.fill_diagonal(Q, -np.sum(Q,1))
 
 # Q = M_data*1 
-ctmc_s, ctmc_t = sim_Q(Q, 100000, 1)
+ctmc_s, ctmc_t = sim_Q(Q, 50000, 1)
 
 # % state back to spikes
 # ctmc_spkt, ctmc_spki = [],[]
@@ -156,7 +172,7 @@ for tt in range(len(ctmc_t)):
 ###############################################################################
 ### Bt_LIF, LIF_sis, LIF_burst
 LIF_isi = []
-for nn in range(N):
+for nn in range(N): #(N):
     pos = np.where(np.array(spike_id)==nn)[0]
     spks = np.array(spike_times)[pos]
     if len(spks)>2:
@@ -189,7 +205,7 @@ ctmc_data, ctmc_ids = [], []
 ctmc_burst = np.ones(4)
 Bt_ctmc = []
 for rr in range(reps_ctmc):
-    ctmc_s, ctmc_t = sim_Q(Q, 50000, 1)
+    ctmc_s, ctmc_t = sim_Q(Q, 10000, 1)
     ctmc_spkt, ctmc_spki = [],[]
     btt = []
     for tt in range(1, len(ctmc_t)):
@@ -228,7 +244,7 @@ def compute_average_acf(time_series_list, max_lag):
 max_lag = 500
 ctmc_acf = compute_average_acf(Bt_ctmc, max_lag)
 LIF_acf = compute_average_acf([Bt_LIF], max_lag)
-lag = np.arange(max_lag + 1)
+lag = np.arange(max_lag + 1)*dt 
 plt.figure()
 plt.plot(lag, ctmc_acf, label='CTMC')
 plt.plot(lag, LIF_acf, label='LIF')
@@ -236,10 +252,10 @@ plt.xlabel('Lag (ms)', fontsize=20); plt.ylabel('acf', fontsize=20); plt.legend(
 
 # %% compare ISI
 ctmc_isi = []
-for rr in range(reps_ctmc):
+for rr in range(len(ctmc_data)):
     spkt = ctmc_data[rr]
     spki = ctmc_ids[rr]
-    for nn in range(N):
+    for nn in range(N): #(N):
         pos = np.where(spki==nn)[0]
         spks = spkt[pos]
         if len(spks)>2:
@@ -251,10 +267,11 @@ plt.figure()
 plt.hist(ctmc_isi, np.arange(0,1,.05)*500, density=True, alpha=0.5, label='CTMC')
 plt.hist(LIF_isi, np.arange(0,1,.05)*500, density=True, alpha=0.5, label='LIF')
 plt.legend(fontsize=20); plt.xlabel('ISI (ms)', fontsize=20)
+plt.yscale('log')
 
 # %% by eye
 # %% LIF
-show_t = 50000  # 5s
+show_t = 10000  # 5s
 plt.figure()
 for tt in range(show_t-1):
     plt.plot(firing[tt][0], firing[tt][1],'k.')
@@ -271,4 +288,27 @@ for tt in range(len(ctmc_t_plt)):
         if word[nn]==1:
             plt.plot(time, nn, 'k.')
 plt.title('CTMC', fontsize=20)
-         
+
+# %% checking weight inference
+###############################################################################
+# %%
+f1,f2,f3 = M_inf[0,4], M_inf[0,2], M_inf[0,1]
+w12,w13,w21 = np.log(M_inf[4,6]/f2), np.log(M_inf[4,5]/f3), np.log(M_inf[2,6]/f1)
+w23,w32,w31 = np.log(M_inf[2,3]/f3), np.log(M_inf[1,3]/f2), np.log(M_inf[1,5]/f1)
+inf_w = np.array([w12,w13,w21,w23,w32,w31])
+
+# %% plotting weights
+bar_width = 0.35
+bar_positions_group2 = np.arange(6)
+bar_positions_group1 = ['w12','w13','w21','w23','w32','w31']
+
+plt.figure()
+plt.subplot(211)
+plt.bar(bar_positions_group1, [S[1,0],S[2,0],S[0,1],S[2,1],S[1,2],S[0,2]], width=bar_width)
+plt.plot(bar_positions_group1, bar_positions_group2*0, 'k')
+plt.ylabel('true weights', fontsize=20)
+
+plt.subplot(212)
+plt.bar(bar_positions_group1, np.array([w12,w13,w21,w23,w32,w31])+0, width=bar_width)
+plt.plot(bar_positions_group1, bar_positions_group2*0, 'k')
+plt.ylabel('MaxCal inferred', fontsize=20)
