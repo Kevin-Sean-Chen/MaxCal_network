4e27c57e8984cf84cf7a5854e832b987d36ef3c1
Kevin-Sean-Chen <kschen@princeton.edu>
2024-11-24 00:30:29 -0500

    revisting R2 and cosine analysis, for different motif and network differences

--- scan_net.py
+++ scan_net.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar  9 16:04:55 2024
+Created on Sat Nov 23 18:02:44 2024
 
 @author: kevin
 """
@@ -19,12 +19,14 @@ matplotlib.rc('xtick', labelsize=20)
 matplotlib.rc('ytick', labelsize=20)
 
 # %%
-def LIF_firing(synaptic_weights, noise_amp, syn3_tau=5):
+def LIF_firing(synaptic_weights, noise_amp, syn_delay=None, syn_ratio=None):
     """
     given synaptic weights and noise amplitude, turn 3-neuron spiking time series
     """
     dt = 0.1  # time step in milliseconds
-    timesteps = 50000  # total simulation steps
+    timesteps = 100000  # total simulation steps
+    if syn_delay is not None:
+        delay_buffer = np.zeros((1, syn_delay))
 
     # Neuron parameters
     tau = 10.0  # membrane time constant
@@ -39,16 +41,30 @@ def LIF_firing(synaptic_weights, noise_amp, syn3_tau=5):
     # synaptic_weights = (np.random.rand(3,3)+1)*20
     # sign = np.random.randn(3,3); sign[sign>0]=1; sign[sign<0] = -1
     # synaptic_weights = synaptic_weights*sign
+    
+    ### rescaling
+    if syn_ratio is not None:
+        synaptic_weights[2,1]*=syn_ratio
+        # kk = list(test.keys())[0]
+        # vv = list(test.values())[0]
+        # if kk=='EI':
+        #     synaptic_weights[2,1]*=vv
+        # if kk=='comm':
+        #     synaptic_weights[2,0]*=vv
+        # if kk=='conv':
+        #     synaptic_weights[2,0]*=vv
+            
     S = synaptic_weights*1
     np.fill_diagonal(S, np.zeros(3))
     # noise_amp = 2
 
     # Synaptic filtering parameters
-    tau_synaptic = np.array([5, 5, syn3_tau])  #5.0  # synaptic time constant
+    tau_synaptic = np.array([5, 5, 5])  #5.0  # synaptic time constant
 
     # Initialize neuron membrane potentials and synaptic inputs
     v_neurons = np.zeros((3, timesteps))
     synaptic_inputs = np.zeros((3, timesteps))
+    spikes = np.zeros((3, timesteps))   ### full array to record spikes
     spike_times = []
     firing = []
     firing.append((np.array([]), np.array([]))) # init
@@ -61,11 +77,21 @@ def LIF_firing(synaptic_weights, noise_amp, syn3_tau=5):
         
         # Check for spikes
         spike_indices = np.where(v_neurons[:, t] > v_threshold)[0]
+        spikes[spike_indices, t] = 1
         
         # Apply synaptic connections with synaptic filtering
+        synaptic_input = synaptic_weights @ spikes[:, t-1]
+        if syn_delay is not None:
+            ### place buffer to handle delayed spikes
+            delay_buffer = np.roll(delay_buffer, -1)
+            delay_buffer[0, -1] = spikes[1, t-1]  # Add latest spike from Neuron 2
+            synaptic_input[2] += 1. * delay_buffer[0, 0]  ### delayed to the third neuron
         synaptic_inputs[:, t] = synaptic_inputs[:, t-1] + dt*( \
-                                -synaptic_inputs[:, t-1]/tau_synaptic + np.sum(synaptic_weights[:, spike_indices], axis=1))
+                                -synaptic_inputs[:, t-1]/tau_synaptic + synaptic_input)
+        # synaptic_inputs[:, t] = synaptic_inputs[:, t-1] + dt*( \
+        #                         -synaptic_inputs[:, t-1]/tau_synaptic + np.sum(synaptic_weights[:, spike_indices], axis=1))
         # synaptic_inputs[:, t] = np.sum(synaptic_weights[:, spike_indices], axis=1)
+        
         # Update membrane potentials with synaptic inputs
         v_neurons[:, t] += synaptic_inputs[:, t]*dt
         
@@ -85,44 +111,56 @@ nc = 2**3
 num_params = int((N*2**N)) 
 spins = [0,1]  # binary patterns
 combinations = list(itertools.product(spins, repeat=N))  # possible configurations
-lt = 50000
+lt = 100000
 
 # %% scanning loop
-w_s = np.array([5,10,20,40])
-# w_s = np.array([5,30,60,90])  # for delay
-n_s = np.array([2,4,8,16])
-R2s = np.zeros((len(w_s), len(n_s)))
+w_s = np.array([1,2,4,8,16])  ### for network strength
+n_s = np.array([1,2,4,8,16])  ### for noist stength
+d_s = np.array([5,30,60,90, 120])  ### for synaptic delay  (pick one neuron for delay)
+h_s = np.array([1,2,4,8,16])  ### for heterogeneious-ness (pick a pair to rescale)
+
+fault_w = 20  ### defualt variables if not tuned
+fault_n = 2
+fault_d = 0
+fault_h = 0
+
+R2s = np.zeros((3, len(n_s)))  ### measure 3 motifs as a function of control
 signs = R2s*0
 coss = R2s*0
 
 ### EI balanced
-Wij = np.array([[0, 1, -2],  # Neuron 0 connections
+Wij_ei = np.array([[0, 1, -2],  # Neuron 0 connections
                 [1, 0, -2],  # Neuron 1 connections
                 [1, 1, 0]])
 ### common-input
-# Wij = np.array([[0, 0, 0],  # Neuron 0 connections
-#                 [1, 0, 0],  # Neuron 1 connections
-                # [1, 0, 0]])
-### convernged with different strength
-# Wij = np.array([[0, 0, 0],  # Neuron 0 connections
+Wij_common = np.array([[0, 0, 0],  # Neuron 0 connections
+                [1, 0, 0],  # Neuron 1 connections
+                [1, 0, 0]])
+## convernged with different strength
+# Wij_converge = np.array([[0, 0, 0],  # Neuron 0 connections
 #                 [0, 0, 0],  # Neuron 1 connections
 #                 [10, 1, 0]])
+Wij_converge = np.array([[0, 0, 0],  # Neuron 0 connections
+                [0, 0, 0],  # Neuron 1 connections
+                [1, 1, 0]])
+
+Ws = (Wij_ei, Wij_common, Wij_converge)  # list of motif types
 
 P0 = np.ones((nc,nc))  # uniform prior
 np.fill_diagonal(P0, np.zeros(nc))
 np.fill_diagonal(P0, -np.sum(P0,1))
 dofs_all = nc**2 + nc
 Cp_condition = np.ones(dofs_all)
-# cutoff = 20
-# Cp_condition[cutoff:] = np.zeros(dofs_all-cutoff) # test with low-d dof constraints!!!
 
-for ww in range(len(w_s)):
-    for nn in range(len(n_s)):
-        print(ww); print(nn)
-        S = Wij*40#w_s[ww]
-        firing = LIF_firing(S, n_s[nn], w_s[ww])
+
+for ww in range(3):
+    Wij = Ws[ww]  ### asign motif
+    for ii in range(len(d_s)):
+        print(ww); print(ii)
+        S = Wij*fault_w #w_s[ii]
+        firing = LIF_firing(S, fault_n, syn_delay=None, syn_ratio=h_s[ii])
         minisi = compute_min_isi(firing)
-        adapt_window = int(minisi*10)  #100
+        adapt_window = 100 #int(minisi*10)  #100
         spk_states, spk_times = spk2statetime(firing, adapt_window)  # embedding states
         tau,C = compute_tauC(spk_states, spk_times)  # emperical measurements
         ### not scanning for now...
@@ -150,39 +188,14 @@ for ww in range(len(w_s)):
         true_s = np.array([S[1,0],S[2,0],S[0,1],S[2,1],S[1,2],S[0,2]])
 
         correlation_coefficient, _ = pearsonr(inf_w, true_s)
-        R2s[ww,nn] = correlation_coefficient
-        signs[ww,nn] = corr_param(true_s, inf_w, 'binary')
-        coss[ww,nn] = cos_ang(inf_w, true_s)
+        R2s[ww, ii] = correlation_coefficient
+        signs[ww, ii] = corr_param(true_s, inf_w, 'binary')
+        coss[ww, ii] = cos_ang(inf_w, true_s)
         print(correlation_coefficient)
         
 # %%
+scan_x = h_s*1
 plt.figure()
-img = plt.imshow(R2s, aspect='auto',  origin='lower');
-cbar = plt.colorbar(img)
-# img.set_clim(0.5, .7) 
-plt.xticks(np.arange(len(n_s)), n_s)
-plt.yticks(np.arange(len(w_s)), w_s)
-plt.xlabel('noise', fontsize=20); plt.ylabel('network', fontsize=20); 
-plt.title('R2', fontsize=20)
-# plt.title('R2 dof:{}'.format(cutoff), fontsize=20)
-# plt.savefig('spk_scan.pdf')
-
-# %%
-plt.figure()
-img = plt.imshow(signs, aspect='auto',  origin='lower');
-cbar = plt.colorbar(img) # img.set_clim(0.5, .7) 
-plt.xticks(np.arange(len(n_s)), n_s); plt.yticks(np.arange(len(w_s)), w_s)
-plt.xlabel('noise', fontsize=20); plt.ylabel('network', fontsize=20); plt.title('signed R2', fontsize=20)
-plt.figure()
-img = plt.imshow(coss, aspect='auto',  origin='lower');
-cbar = plt.colorbar(img) # img.set_clim(0.5, .7) 
-plt.xticks(np.arange(len(n_s)), n_s); plt.yticks(np.arange(len(w_s)), w_s)
-plt.xlabel('noise', fontsize=20); plt.ylabel('network', fontsize=20); plt.title('cos-angle', fontsize=20)
-
-# %% IDEAS
-# alter window according to ISI in data
-# early cutoff for constraints!
-#... next:
-# play with motif (cyclic and mutual inhibition)
-# download retina
-
+plt.subplot(131); plt.semilogx(scan_x, R2s.T,'-o', label=['EI', 'comm', 'conv']); plt.title('R2'); plt.legend()
+plt.subplot(132); plt.semilogx(scan_x, signs.T,'-o', label=['EI', 'comm', 'conv']); plt.title('signed corr');plt.xlabel('difference')
+plt.subplot(133); plt.semilogx(scan_x, coss.T,'-o', label=['EI', 'comm', 'conv']); plt.title('cosine')
